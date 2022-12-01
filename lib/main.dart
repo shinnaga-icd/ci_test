4e4b973bbd15e86d20c2453dab154da762f44f26
shinnaga-icd <shinji.nagasaki@infcurion.com>
2022-12-01 12:18:07 +0900

    j

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Test a',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.deepOrange,
       ),
       home: const MyHomePage(title: 'Test'),
     );
