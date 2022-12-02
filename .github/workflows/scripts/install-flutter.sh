3e9d9a06f24741a21b5ecb0f505ae7b7b055594b
shinnaga-icd <shinji.nagasaki@infcurion.com>
2022-12-02 16:56:03 +0900

    a

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# 引数処理 v:flutter vesion, p:flutter setup path
+while getopts v:p: OPT
+do
+  case $OPT in
+    "v" ) VER="$OPTARG" ;;
+    "p" ) SET_PATH="$OPTARG" ;;
+  esac
+done
+
+
 sudo apt update && sudo apt upgrade -y && sudo apt install -y ninja-build libgtk-3-dev
 
 # 参考
 # https://zenn.dev/k9i/articles/65ed8841d5e3ff
 
-#BRANCH=$1
-BRANCH=3.0.4
-git clone https://github.com/flutter/flutter.git --depth 1 -b $BRANCH "$GITHUB_WORKSPACE/_flutter"
+git clone https://github.com/flutter/flutter.git --depth 1 -b $VER "$GITHUB_WORKSPACE/_flutter"
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
 
 #wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.0.2-stable.tar.xz
