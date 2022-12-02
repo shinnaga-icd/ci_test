878c8d0cc604c27ee96ec3a78461b80f9e7167d5
shinnaga-icd <shinji.nagasaki@infcurion.com>
2022-12-02 17:03:18 +0900

    b

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -15,8 +15,8 @@ sudo apt update && sudo apt upgrade -y && sudo apt install -y ninja-build libgtk
 # 参考
 # https://zenn.dev/k9i/articles/65ed8841d5e3ff
 
-git clone https://github.com/flutter/flutter.git --depth 1 -b $VER "$GITHUB_WORKSPACE/_flutter"
-echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
+git clone https://github.com/flutter/flutter.git --depth 1 -b $VER $SET_PATH
+echo "$SET_PATH/bin" >> $GITHUB_PATH
 
 #wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.0.2-stable.tar.xz
 #tar xf flutter_linux_3.0.2-stable.tar.xz
