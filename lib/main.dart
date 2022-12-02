e7903033fa4ef6e22634ee855a765a719df0ec3a
shinnaga-icd <shinji.nagasaki@infcurion.com>
2022-12-02 13:40:36 +0900

    e

--- lib/main.dart
+++ lib/main.dart
@@ -109,7 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.abc),
+        child: const Icon(Icons.abc_outlined),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
